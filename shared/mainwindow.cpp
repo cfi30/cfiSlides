cfi30/cfiSlides

commit 8285149b9b1653b22ce1704dd63c7db96ff13089

add selection actions to the context menu

shared/mainwindow.cpp
@@ -444,7 +444,7 @@ void MainWindow::displaySlide(Slide *slide)
 
 	GraphicsView *view = new GraphicsView(scene);
 	view->setContextMenuPolicy(Qt::CustomContextMenu);
-	connect(view, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(showViewContextMenu(const QPoint &)));
+	connect(view, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(displayContextMenu(const QPoint &)));
 	ui->displayWidget->addWidget(view);
 
 	QPixmap pixmap(scene->sceneRect().size().toSize());
@@ -1167,15 +1167,28 @@ void MainWindow::moveFinishTimerTimeout()
 	updateCurrentPropertiesEditor();
 }
 
-void MainWindow::showViewContextMenu(const QPoint &pos)
+void MainWindow::displayContextMenu(const QPoint &pos)
 {
 	GraphicsView *view = qobject_cast<GraphicsView *>(sender());
-	QPoint globalPos = view->mapToGlobal(pos);
+	QGraphicsItem *item = view->scene()->itemAt(view->mapToScene(pos));
+	while(item->parentItem())
+		item = item->parentItem();
+	if(item->flags().testFlag(QGraphicsItem::ItemIsSelectable))
+	{
+		view->scene()->clearSelection();
+		item->setSelected(true);
+	}
+
 	QMenu *menu = new QMenu(this);
+	if(!view->scene()->selectedItems().isEmpty())
+	{
+		menu->addActions(selectionActions->actions());
+		menu->addSeparator();
+	}
 	menu->addAction(ui->actionSelectAll);
 	menu->addAction(ui->actionRepaint);
 	setInsertElemMenu(menu);
-	menu->exec(globalPos);
+	menu->exec(view->mapToGlobal(pos));
 	delete menu;
 }
 

shared/mainwindow.h
@@ -113,7 +113,7 @@ public slots:
 	void managePlugins();
 	void print();
 	void moveFinishTimerTimeout();
-	void showViewContextMenu(const QPoint &);
+	void displayContextMenu(const QPoint &);
 	void loadPlugins();
 	void unloadPlugins();
 	void aboutPlugins();

commit 9e340c7a8bb8b0b8fc718dc0010bba897fe13995

fix a segfault introduced in 8285149b9b1653b22ce1704dd63c7db96ff13089

shared/mainwindow.cpp
@@ -1187,12 +1187,17 @@ void MainWindow::displayContextMenu(const QPoint &pos)
 {
 	GraphicsView *view = qobject_cast<GraphicsView *>(sender());
 	QGraphicsItem *item = view->scene()->itemAt(view->mapToScene(pos));
-	while(item->parentItem())
-		item = item->parentItem();
-	if(item->flags().testFlag(QGraphicsItem::ItemIsSelectable))
+
+	if(item)
 	{
-		view->scene()->clearSelection();
-		item->setSelected(true);
+		while(item->parentItem())
+			item = item->parentItem();
+
+		if(item->flags().testFlag(QGraphicsItem::ItemIsSelectable))
+		{
+			view->scene()->clearSelection();
+			item->setSelected(true);
+		}
 	}
 
 	QMenu *menu = new QMenu(this);