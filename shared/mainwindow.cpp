cfi30/cfiSlides

commit e5e8320a3ef6b51e96968abf128b86fa035126be

extend slide's context menu to the element list

shared/mainwindow.cpp
@@ -406,7 +406,7 @@ void MainWindow::displaySlide(Slide *slide)
 
 	GraphicsView *view = new GraphicsView(scene);
 	view->setContextMenuPolicy(Qt::CustomContextMenu);
-	connect(view, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(displayContextMenu(const QPoint &)));
+	connect(view, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(displayViewContextMenu(const QPoint &)));
 	ui->displayWidget->addWidget(view);
 
 	QPixmap pixmap(scene->sceneRect().size().toSize());
@@ -1031,7 +1031,26 @@ void MainWindow::moveFinishTimerTimeout()
 	updateCurrentPropertiesEditor();
 }
 
-void MainWindow::displayContextMenu(const QPoint &pos)
+QMenu *MainWindow::createSlideContextMenu()
+{
+	QMenu *menu = new QMenu(this);
+	if(!ui->slideTree->selectedItems().isEmpty())
+	{
+		menu->addActions(selectionActions->actions());
+		menu->addSeparator();
+	}
+	menu->addAction(ui->actionSelectAll);
+	menu->addAction(ui->actionRepaint);
+
+	QAction *separator = menu->addSeparator();
+	separator->setText(ui->menuInsertElement->menuAction()->text());
+	separator->setVisible(true);
+
+	menu->addActions(insertActions);
+	return menu;
+}
+
+void MainWindow::displayViewContextMenu(const QPoint &pos)
 {
 	GraphicsView *view = qobject_cast<GraphicsView *>(sender());
 	QGraphicsItem *item = view->scene()->itemAt(view->mapToScene(pos));
@@ -1048,22 +1067,25 @@ void MainWindow::displayContextMenu(const QPoint &pos)
 		}
 	}
 
-	QMenu *menu = new QMenu(this);
-	if(!view->scene()->selectedItems().isEmpty())
-	{
-		menu->addActions(selectionActions->actions());
-		menu->addSeparator();
-	}
-	menu->addAction(ui->actionSelectAll);
-	menu->addAction(ui->actionRepaint);
-
-	QAction *separator = menu->addSeparator();
-	separator->setText(ui->menuInsertElement->menuAction()->text());
-	separator->setVisible(true);
-
-	menu->addActions(insertActions);
-
+	QMenu *menu = createSlideContextMenu();
 	menu->exec(view->mapToGlobal(pos));
+	delete menu;
+}
+
+void MainWindow::displaySlideTreeContextMenu(const QPoint &pos)
+{
+	if(ui->slideTree->selectedItems().isEmpty())
+	{
+		QTreeWidgetItem *item = ui->slideTree->itemAt(pos);
+		if(item && item->flags().testFlag(Qt::ItemIsSelectable))
+		{
+			ui->slideTree->clearSelection();
+			item->setSelected(true);
+		}
+	}
+
+	QMenu *menu = createSlideContextMenu();
+	menu->exec(ui->slideTree->mapToGlobal(pos));
 	delete menu;
 }
 

shared/mainwindow.h
@@ -113,7 +113,6 @@ public slots:
 	void managePlugins();
 	void print();
 	void moveFinishTimerTimeout();
-	void displayContextMenu(const QPoint &);
 	void loadPlugins();
 	void unloadPlugins();
 	void aboutPlugins();
@@ -122,7 +121,7 @@ public slots:
 	void openRecentFile(QAction *);
 	void displayInsertElemMenu();
 	void resizeSlideshow();
-
+	QMenu *createSlideContextMenu();
 	void currentSlideChanged(int currentRow);
 	void slideItemChanged(QListWidgetItem *item);
 	void elementItemChanged(QTreeWidgetItem *item, int column);
@@ -137,6 +136,8 @@ public slots:
 	void duplicateElements();
 
 private slots:
+	void displayViewContextMenu(const QPoint &);
+	void displaySlideTreeContextMenu(const QPoint &);
 	void insertElementFromAction();
 
 private:

shared/mainwindow.ui
@@ -311,6 +311,9 @@
      </property>
      <item>
       <widget class="QTreeWidget" name="slideTree">
+       <property name="contextMenuPolicy">
+        <enum>Qt::CustomContextMenu</enum>
+       </property>
        <property name="selectionMode">
         <enum>QAbstractItemView::MultiSelection</enum>
        </property>
@@ -1454,6 +1457,22 @@
     </hint>
    </hints>
   </connection>
+  <connection>
+   <sender>slideTree</sender>
+   <signal>customContextMenuRequested(QPoint)</signal>
+   <receiver>MainWindow</receiver>
+   <slot>displaySlideTreeContextMenu(QPoint)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>127</x>
+     <y>109</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>199</x>
+     <y>154</y>
+    </hint>
+   </hints>
+  </connection>
  </connections>
  <slots>
   <slot>createEmptySlide()</slot>
@@ -1485,5 +1504,6 @@
   <slot>bringElementToBack()</slot>
   <slot>bringElementToFront()</slot>
   <slot>resizeSlideshow()</slot>
+  <slot>displaySlideTreeContextMenu(QPoint)</slot>
  </slots>
 </ui>

commit e718d2bee9010c2cf3bc863023bee29f44d065ee

fix a crash introduced in e5e8320a3ef6b51e96968abf128b86fa035126be

shared/mainwindow.cpp
@@ -1074,6 +1074,9 @@ void MainWindow::displayViewContextMenu(const QPoint &pos)
 
 void MainWindow::displaySlideTreeContextMenu(const QPoint &pos)
 {
+	if(ui->slideList->currentRow() == -1)
+		return;
+
 	if(ui->slideTree->selectedItems().isEmpty())
 	{
 		QTreeWidgetItem *item = ui->slideTree->itemAt(pos);