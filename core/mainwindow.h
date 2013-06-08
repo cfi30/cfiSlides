cfi30/cfiSlides

commit 340a2de0108f926baa1c52c5046a6dea92d9fdfb

move multiple objects to 'core/' for a better structure

core/mainwindow.h
@@ -55,14 +55,13 @@
 #include "plugin.h"
 #include "geometrydialog.h"
 #include "configuration.h"
-#include "shared.h"
 
 namespace Ui
 {
 	class MainWindow;
 }
 
-class CFISLIDES_DLLSPEC MainWindow : public QMainWindow
+class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 
@@ -72,23 +71,14 @@ public:
 	void setWindowModified(const bool);
 	void setWindowTitle(const QString &);
 
-	Slideshow *getSlideshow() const;
-	void displaySlideshow();
-	bool closeSlideshow();
-	void displaySlide(Slide *slide);
-	void updateSlide(const int index);
-	void updateIcon(const int index);
-	void updateSlideTree(const int index);
-	void updatePropertiesEditor(const SlideshowElement *element);
-	QGraphicsItem *sceneItemFromIndex(const int index) const;
-	void insertElement(SlideElement *element);
-	void moveElement(const int before, const int after);
-
-	static bool validateSlideName(const QString &name);
-	static bool validateElementName(const QString &name);
-
-	void registerElementType(const int typeId, const QString &label, const QIcon &icon = QIcon());
-	void unregisterElementType(const int typeId);
+	Q_INVOKABLE Slideshow *getSlideshow() const;
+	Q_INVOKABLE bool closeSlideshow();
+	Q_INVOKABLE void displaySlide(Slide *slide);
+	Q_INVOKABLE void updateSlide(const int index);
+	Q_INVOKABLE void updateIcon(const int index);
+	Q_INVOKABLE void updateSlideTree(const int index);
+	Q_INVOKABLE QGraphicsItem *sceneItemFromIndex(const int index) const;
+	Q_INVOKABLE void insertElement(SlideElement *element);
 
 public slots:
 	void newSlideshow();
@@ -113,7 +103,6 @@ public slots:
 	void managePlugins();
 	void print();
 	void moveFinishTimerTimeout();
-	QMenu *createSlideContextMenu();
 	void loadPlugins();
 	void unloadPlugins();
 	void aboutPlugins();
@@ -136,16 +125,16 @@ public slots:
 	void moveSlideRight();
 	void duplicateElements();
 
-private slots:
-	void displayViewContextMenu(const QPoint &);
-	void displaySlideTreeContextMenu(const QPoint &);
-	void displaySlideListContextMenu(const QPoint &pos);
-	void insertElementFromAction();
-
 private:
-	Ui::MainWindow *ui;
+	void updatePropertiesEditor(const SlideshowElement *element);
+	void moveElement(const int before, const int after);
+	bool validateSlideName(const QString &name);
+	bool validateElementName(const QString &name);
+	void registerElementType(const SlideElementType &type);
+	void unregisterElementType(const SlideElementType &type);
+	QMenu *createSlideContextMenu();
 
-protected:
+	Ui::MainWindow *ui;
 	Slideshow *slideshow;
 	int newSlideshowCount;
 	QTimer moveFinishTimer;
@@ -154,6 +143,15 @@ protected:
 	QString commandLineHelp;
 	QActionGroup *currentSlideActions;
 	QActionGroup *selectionActions;
+	SlideElementTypeList pluginTypes;
+
+private slots:
+	void displayViewContextMenu(const QPoint &);
+	void displaySlideTreeContextMenu(const QPoint &);
+	void displaySlideListContextMenu(const QPoint &pos);
+	void insertElementFromAction();
+
+protected:
 	virtual void closeEvent(QCloseEvent *);
 };
 

plugins/importexport/importexport.cpp
@@ -66,7 +66,10 @@ QString ImportExport::about() const
 
 void ImportExport::launchImport()
 {
-	ImportDialog *dialog = new ImportDialog(window->getSlideshow()->getSlides().count(), window);
+	Slideshow *slideshow;
+	QMetaObject::invokeMethod(window, "getSlideshow", Qt::DirectConnection, Q_RETURN_ARG(Slideshow *, slideshow));
+
+	ImportDialog *dialog = new ImportDialog(slideshow->getSlides().count(), window);
 	connect(dialog, SIGNAL(finished(int)), dialog, SLOT(deleteLater()));
 
 	if(dialog->exec() == QDialog::Rejected)
@@ -88,13 +91,13 @@ void ImportExport::launchImport()
 		Slide *tempSlide = slides[index];
 		progress->setValue(index);
 
-		Slide *slide = window->getSlideshow()->createSlide("temp-name");
+		Slide *slide = slideshow->createSlide("temp-name");
 		slide->setValues(tempSlide->getValues());
 
 		foreach(SlideElement *element, tempSlide->getElements())
 			slide->addElement(element);
 
-		window->displaySlide(slide);
+		QMetaObject::invokeMethod(window, "displaySlide", Qt::DirectConnection, Q_ARG(Slide *, slide));
 		window->setWindowModified(true);
 	}
 
@@ -104,7 +107,10 @@ void ImportExport::launchImport()
 
 void ImportExport::launchExport()
 {
-	const int slideCount = window->getSlideshow()->getSlides().size();
+	Slideshow *slideshow;
+	QMetaObject::invokeMethod(window, "getSlideshow", Qt::DirectConnection, Q_RETURN_ARG(Slideshow *, slideshow));
+
+	const int slideCount = slideshow->getSlides().size();
 	if(slideCount < 1)
 	{
 		QMessageBox::critical(window, exportAction->text(), tr("Impossible d'exporter un diaporama vide."));
@@ -142,8 +148,8 @@ void ImportExport::launchExport()
 	{
 		progress->setValue(index + 1);
 
-		Slide *slide = window->getSlideshow()->getSlide(index);
-		GraphicsView *view = qobject_cast<GraphicsView *>(window->findChild<QStackedWidget *>("displayWidget")->widget(index));
+		Slide *slide = slideshow->getSlide(index);
+		QGraphicsView *view = qobject_cast<QGraphicsView *>(window->findChild<QStackedWidget *>("displayWidget")->widget(index));
 
 		QPixmap pixmap(view->scene()->sceneRect().size().toSize());
 		QPainter painter(&pixmap);

cfiSlides.pro
@@ -7,5 +7,5 @@ SUBDIRS += \
 build-plugins {
 	SUBDIRS += \
 		plugins/helloworld \
-		plugins/importexport
+		plugins/importexport \
 }

core/audioelement.h
@@ -16,42 +16,40 @@
  * along with cfiSlides.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef PLUGIN_H
-#define PLUGIN_H
+#ifndef AudioElement_H
+#define AudioElement_H
 
-#include <QtPlugin>
-#include <QMainWindow>
+#include <phonon/MediaObject>
+#include <phonon/AudioOutput>
 
-#include "slideelementtype.h"
+#include "slideelement.h"
 
-class MainWindow;
-
-class Plugin : public QObject
+class AudioElement : public SlideElement
 {
 	Q_OBJECT
 
 public:
-	Plugin() {}
-	virtual QString name() const = 0;
-	virtual QString version() const = 0;
-	virtual QString author() const = 0;
-	virtual QString about() const = 0;
-	virtual SlideElementTypeList getElementTypes() const
-	{
-		return SlideElementTypeList();
-	}
+	AudioElement();
+	virtual QString previewUrl() const;
+	virtual void render(QGraphicsScene *scene, const bool interactive);
+	virtual PropertyList getProperties() const;
 
-	void setWindow(QMainWindow *window)
-	{
-		this->window = window;
-		initialize();
-	}
+public slots:
+	virtual void play();
+	virtual void pause();
+	virtual void stop();
+	virtual void toggleMute();
+	virtual void destroy();
+
+private slots:
+	void restart();
 
 protected:
-	QMainWindow *window;
-	virtual void initialize() = 0;
+	Phonon::MediaObject *mediaObject;
+	Phonon::AudioOutput *audioOutput;
+	bool playbackFinished;
 };
 
-Q_DECLARE_INTERFACE(Plugin, "cfiSlides.Plugin")
+Q_DECLARE_METATYPE(AudioElement)
 
-#endif // PLUGIN_H
+#endif // AudioElement_H

core/core.pro
@@ -1,4 +1,4 @@
-QT += core gui
+QT += core gui phonon
 TARGET = cfiSlides
 TEMPLATE = app
 DESTDIR = ../build/
@@ -7,8 +7,73 @@ LIBDIR = $$DESTDIR
 VERSION = 0.3
 DEFINES += CFISLIDES_VERSION=\\\"$$VERSION\\\"
 
+HEADERS += \
+	mainwindow.h \
+	slideshow.h \
+	slide.h \
+	graphicsview.h \
+	baseelement.h \
+	slideelement.h \
+	imageelement.h \
+	graphicsitem.h \
+	graphicspixmapitem.h \
+	graphicsrectitem.h \
+	rectelement.h \
+	graphicstextitem.h \
+	textelement.h \
+	movieelement.h \
+	graphicsellipseitem.h \
+	ellipseelement.h \
+	viewwidget.h \
+	audioelement.h \
+	graphicslineitem.h \
+	lineelement.h \
+	plugindialog.h \
+	plugin.h \
+	shared.h \
+	graphicsmoviepreviewitem.h \
+	slideshowelement.h \
+	geometrydialog.h \
+	property.h \
+	propertymanager.h \
+	propertyeditor.h \
+	propertyeditordelegate.h \
+
 SOURCES += \
-	main.cpp
+	main.cpp \
+	mainwindow.cpp \
+	slideshow.cpp \
+	slide.cpp \
+	graphicsview.cpp \
+	baseelement.cpp \
+	slideelement.cpp \
+	imageelement.cpp \
+	graphicspixmapitem.cpp \
+	graphicsrectitem.cpp \
+	rectelement.cpp \
+	graphicstextitem.cpp \
+	textelement.cpp \
+	movieelement.cpp \
+	graphicsellipseitem.cpp \
+	ellipseelement.cpp \
+	viewwidget.cpp \
+	audioelement.cpp \
+	graphicslineitem.cpp \
+	lineelement.cpp \
+	plugindialog.cpp \
+	graphicsmoviepreviewitem.cpp \
+	slideshowelement.cpp \
+	geometrydialog.cpp \
+	property.cpp \
+	propertymanager.cpp \
+	propertyeditor.cpp \
+	propertyeditordelegate.cpp \
+
+FORMS += \
+	mainwindow.ui \
+	viewwidget.ui \
+	plugindialog.ui \
+	geometrydialog.ui \
 
 TRANSLATIONS = \
 	strings_fr.ts \

core/ellipseelement.h
@@ -16,20 +16,21 @@
  * along with cfiSlides.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef GRAPHICSPIXMAPITEM_H
-#define GRAPHICSPIXMAPITEM_H
+#ifndef ELLIPSEELEMENT_H
+#define ELLIPSEELEMENT_H
 
-#include <QGraphicsPixmapItem>
+#include "rectelement.h"
+#include "graphicsellipseitem.h"
 
-#include "graphicsitem.h"
-#include "shared.h"
-
-class CFISLIDES_DLLSPEC GraphicsPixmapItem : public QGraphicsPixmapItem
+class EllipseElement : public RectElement
 {
-	GRAPHICS_ITEM
+	Q_OBJECT
 
 public:
-	explicit GraphicsPixmapItem(QGraphicsItem *parent = 0);
+	EllipseElement() : RectElement() {}
+	virtual void render(QGraphicsScene *scene, const bool interactive);
 };
 
-#endif // GRAPHICSPIXMAPITEM_H
+Q_DECLARE_METATYPE(EllipseElement)
+
+#endif // ELLIPSEELEMENT_H

commit 22fc9424ecf70d4f4bc84b77b3ddaa45774feca0

use the right setWindowModified in the ImportExport plugin (glitch introduced in 340a2de0108f926baa1c52c5046a6dea92d9fdfb)

core/mainwindow.h
@@ -68,9 +68,9 @@ class MainWindow : public QMainWindow
 public:
 	explicit MainWindow(QString commandLineHelp = QString(), QString openFile = QString(), bool disablePlugins = true, QWidget *parent = 0);
 	~MainWindow();
-	void setWindowModified(const bool);
-	void setWindowTitle(const QString &);
 
+	Q_INVOKABLE void setWindowModified(const bool);
+	Q_INVOKABLE void setWindowTitle(const QString &);
 	Q_INVOKABLE Slideshow *getSlideshow() const;
 	Q_INVOKABLE bool closeSlideshow();
 	Q_INVOKABLE void displaySlide(Slide *slide);

plugins/importexport/importexport.cpp
@@ -98,7 +98,7 @@ void ImportExport::launchImport()
 			slide->addElement(element);
 
 		QMetaObject::invokeMethod(window, "displaySlide", Qt::DirectConnection, Q_ARG(Slide *, slide));
-		window->setWindowModified(true);
+		QMetaObject::invokeMethod(window, "setWindowModified", Qt::DirectConnection, Q_ARG(bool, true));
 	}
 
 	progress->close();