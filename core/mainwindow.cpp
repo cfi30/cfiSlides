cfi30/cfiSlides

commit 55c43b6c49dc9068de811ee25ae48ccde0828154

using the new signal/slot syntax

core/mainwindow.cpp
@@ -66,12 +66,12 @@ MainWindow::MainWindow(QString commandLineHelp, QString openFile, bool disablePl
 	ui->mediaDock->setWindowTitle(ui->actionMediaDock->text());
 
 	// this doesn't work when using the designer
-	connect(ui->menuRecentFiles, SIGNAL(aboutToShow()), this, SLOT(displayRecentFiles()));
-	connect(ui->menuRecentFiles, SIGNAL(triggered(QAction*)), this, SLOT(openRecentFile(QAction*)));
+	connect(ui->menuRecentFiles, &QMenu::aboutToShow, this, &MainWindow::displayRecentFiles);
+	connect(ui->menuRecentFiles, &QMenu::triggered, this, &MainWindow::openRecentFile);
 
 	ui->actionCurrentSlide->setSeparator(true);
 
-	connect(ui->menuInsertElement, SIGNAL(aboutToShow()), this, SLOT(displayInsertElemMenu()));
+	connect(ui->menuInsertElement, &QMenu::aboutToShow, this, &MainWindow::displayInsertElemMenu);
 
 	currentSlideActions = new QActionGroup(this);
 	currentSlideActions->addAction(ui->menuInsertElement->menuAction());
@@ -99,10 +99,10 @@ MainWindow::MainWindow(QString commandLineHelp, QString openFile, bool disablePl
 	ui->volumeSlider->setValue(previewPlayer->volume());
 	ui->pauseButton->hide();
 
-	connect(previewPlayer, SIGNAL(seekableChanged(bool)), this, SLOT(previewSeekableChanged(bool)));
-	connect(previewPlayer, SIGNAL(durationChanged(qint64)), this, SLOT(previewDurationChanged(qint64)));
-	connect(previewPlayer, SIGNAL(positionChanged(qint64)), this, SLOT(previewPositionChanged(qint64)));
-	connect(previewPlayer, SIGNAL(stateChanged(QMediaPlayer::State)), this, SLOT(previewStateChanged(QMediaPlayer::State)));
+	connect(previewPlayer, &QMediaPlayer::seekableChanged, this, &MainWindow::previewSeekableChanged);
+	connect(previewPlayer, &QMediaPlayer::durationChanged, this, &MainWindow::previewDurationChanged);
+	connect(previewPlayer, &QMediaPlayer::positionChanged, this, &MainWindow::previewPositionChanged);
+	connect(previewPlayer, &QMediaPlayer::stateChanged, this, &MainWindow::previewStateChanged);
 
 	this->slideshow = 0;
 	this->newSlideshowCount = 0;
@@ -116,7 +116,7 @@ MainWindow::MainWindow(QString commandLineHelp, QString openFile, bool disablePl
 
 	moveFinishTimer.setInterval(REFRESH_INTERVAL);
 	moveFinishTimer.setSingleShot(true);
-	connect(&moveFinishTimer, SIGNAL(timeout()), this, SLOT(moveFinishTimerTimeout()));
+	connect(&moveFinishTimer, &QTimer::timeout, this, &MainWindow::moveFinishTimerTimeout);
 
 	if(disablePlugins)
 	{
@@ -125,7 +125,7 @@ MainWindow::MainWindow(QString commandLineHelp, QString openFile, bool disablePl
 		statusBar()->showMessage(tr("Extensions désactivés par la ligne de commandes."));
 	}
 	else
-		QTimer::singleShot(REFRESH_INTERVAL, this, SLOT(loadPlugins()));
+		QTimer::singleShot(REFRESH_INTERVAL, this, SLOT(loadPlugins));
 }
 
 MainWindow::~MainWindow()
@@ -423,14 +423,14 @@ void MainWindow::displaySlide(Slide *slide)
 	QGraphicsScene *scene = new QGraphicsScene();
 	scene->setSceneRect(slideshow->getValue("geometry", QDesktopWidget().screenGeometry()).toRect());
 	scene->setItemIndexMethod(QGraphicsScene::NoIndex);
-	connect(scene, SIGNAL(selectionChanged()), this, SLOT(updateCurrentSlideTree()));
-	connect(scene, SIGNAL(selectionChanged()), this, SLOT(updateSelectionActions()));
-	connect(scene, SIGNAL(selectionChanged()), this, SLOT(updateCurrentPropertiesEditor()));
-	connect(scene, SIGNAL(selectionChanged()), this, SLOT(updateMediaPreview()));
+	connect(scene, &QGraphicsScene::selectionChanged, this, &MainWindow::updateCurrentSlideTree);
+	connect(scene, &QGraphicsScene::selectionChanged, this, &MainWindow::updateSelectionActions);
+	connect(scene, &QGraphicsScene::selectionChanged, this, &MainWindow::updateCurrentPropertiesEditor);
+	connect(scene, &QGraphicsScene::selectionChanged, this, &MainWindow::updateMediaPreview);
 
 	GraphicsView *view = new GraphicsView(scene);
 	view->setContextMenuPolicy(Qt::CustomContextMenu);
-	connect(view, SIGNAL(customContextMenuRequested(const QPoint &)), this, SLOT(displayViewContextMenu(const QPoint &)));
+	connect(view, &QWidget::customContextMenuRequested, this, &MainWindow::displayViewContextMenu);
 	ui->displayWidget->addWidget(view);
 
 	QPixmap pixmap(scene->sceneRect().size().toSize());
@@ -457,9 +457,9 @@ void MainWindow::displaySlide(Slide *slide)
 
 	this->currentSlideActions->setEnabled(true);
 
-	connect(slide, SIGNAL(modified()), this, SLOT(slideModified()));
-	connect(slide, SIGNAL(moved()), this, SLOT(slideElementMoved()));
-	connect(slide, SIGNAL(refresh()), this, SLOT(reRenderSlide()));
+	connect(slide, &SlideshowElement::modified, this, &MainWindow::slideModified);
+	connect(slide, &Slide::moved, this, &MainWindow::slideElementMoved);
+	connect(slide, &Slide::refresh, this, &MainWindow::reRenderSlide);
 
 	statusBar()->clearMessage();
 }
@@ -956,9 +956,9 @@ void MainWindow::launchViewer()
 	}
 
 	ViewWidget *viewer = new ViewWidget();
-	connect(viewer, SIGNAL(closed(int)), this, SLOT(show()));
-	connect(viewer, SIGNAL(closed(int)), ui->slideTree, SLOT(clearSelection()));
-	connect(viewer, SIGNAL(closed(int)), viewer, SLOT(deleteLater()));
+	connect(viewer, &ViewWidget::closed, this, &QMainWindow::show);
+	connect(viewer, &ViewWidget::closed, ui->slideTree, &QTreeWidget::clearSelection);
+	connect(viewer, &ViewWidget::closed, viewer, &QObject::deleteLater);
 	viewer->setSlideshow(this->slideshow, ui->slideList->currentRow());
 	viewer->showFullScreen();
 	this->hide();

core/audioelement.cpp
@@ -38,7 +38,7 @@ void AudioElement::render(QGraphicsScene *scene, const bool interactive)
 
 	player = new QMediaPlayer;
 	player->setVolume(getValue("volume").toInt());
-	connect(player, SIGNAL(stateChanged(QMediaPlayer::State)), this, SLOT(stateChanged(QMediaPlayer::State)));
+	connect(player, &QMediaPlayer::stateChanged, this, &AudioElement::stateChanged);
 
 	QMediaPlaylist *playlist = new QMediaPlaylist(player);
 	playlist->addMedia(QUrl::fromLocalFile(getValue("src").toString()));
@@ -50,13 +50,13 @@ void AudioElement::render(QGraphicsScene *scene, const bool interactive)
 PropertyList AudioElement::getProperties() const
 {
 	BoolPropertyManager *boolManager = new BoolPropertyManager;
-	connect(boolManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(boolManager, &PropertyManager::modified, this, &AudioElement::propertyChanged);
 
 	FilePropertyManager *fileManager = new FilePropertyManager;
-	connect(fileManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(fileManager, &PropertyManager::modified, this, &AudioElement::propertyChanged);
 
 	IntSliderPropertyManager *sliderManager = new IntSliderPropertyManager;
-	connect(sliderManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(sliderManager, &PropertyManager::modified, this, &AudioElement::propertyChanged);
 
 	Property *visible = new Property(boolManager, tr("Activer"), "visible");
 	visible->setToolTip(tr("Activer l'élément"));

core/imageelement.cpp
@@ -83,10 +83,10 @@ void ImageElement::render(QGraphicsScene *scene, const bool interactive)
 PropertyList ImageElement::getProperties() const
 {
 	FilePropertyManager *fileManager = new FilePropertyManager;
-	connect(fileManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(fileManager, &PropertyManager::modified, this, &ImageElement::propertyChanged);
 
 	EnumPropertyManager *enumManager = new EnumPropertyManager;
-	connect(enumManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(enumManager, &PropertyManager::modified, this, &ImageElement::propertyChanged);
 
 	Property *group = new Property(0, tr("Image"));
 

core/lineelement.cpp
@@ -66,19 +66,19 @@ void LineElement::render(QGraphicsScene *scene, const bool interactive)
 PropertyList LineElement::getProperties() const
 {
 	BoolPropertyManager *boolManager = new BoolPropertyManager;
-	connect(boolManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(boolManager, &PropertyManager::modified, this, &LineElement::propertyChanged);
 
 	PointPropertyManager *pointManager = new PointPropertyManager;
-	connect(pointManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(pointManager, &PropertyManager::modified, this, &LineElement::propertyChanged);
 
 	IntPropertyManager *intManager = new IntPropertyManager;
-	connect(intManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(intManager, &PropertyManager::modified, this, &LineElement::propertyChanged);
 
 	ColorPropertyManager *colorManager = new ColorPropertyManager;
-	connect(colorManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(colorManager, &PropertyManager::modified, this, &LineElement::propertyChanged);
 
 	EnumPropertyManager *enumManager = new EnumPropertyManager;
-	connect(enumManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(enumManager, &PropertyManager::modified, this, &LineElement::propertyChanged);
 
 	Property *visible = new Property(boolManager, tr("Visible"), "visible");
 	visible->setToolTip(tr("Visibilité de l'élément"));

core/rectelement.cpp
@@ -48,13 +48,13 @@ void RectElement::render(QGraphicsScene *scene, const bool interactive)
 PropertyList RectElement::getProperties() const
 {
 	EnumPropertyManager *enumManager = new EnumPropertyManager;
-	connect(enumManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(enumManager, &PropertyManager::modified, this, &RectElement::propertyChanged);
 
 	ColorPropertyManager *colorManager = new ColorPropertyManager;
-	connect(colorManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(colorManager, &PropertyManager::modified, this, &RectElement::propertyChanged);
 
 	IntPropertyManager *intManager = new IntPropertyManager;
-	connect(intManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(intManager, &PropertyManager::modified, this, &RectElement::propertyChanged);
 
 	Property *background = new Property(0, tr("Remplissage"));
 

core/textelement.cpp
@@ -42,29 +42,29 @@ void TextElement::render(QGraphicsScene *scene, const bool interactive)
 	item->setTextWidth(getValue("width").toInt());
 	item->setPos(getValue("position").toPoint());
 
-	connect(item->document(), SIGNAL(contentsChanged()), this, SLOT(textChanged()));
+	connect(item->document(), &QTextDocument::contentsChanged, this, &TextElement::textChanged);
 	scene->addItem(item);
 }
 
 PropertyList TextElement::getProperties() const
 {
 	BoolPropertyManager *boolManager = new BoolPropertyManager;
-	connect(boolManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(boolManager, &PropertyManager::modified, this, &TextElement::propertyChanged);
 
 	PointPropertyManager *pointManager = new PointPropertyManager;
-	connect(pointManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(pointManager, &PropertyManager::modified, this, &TextElement::propertyChanged);
 
 	IntPropertyManager *intManager = new IntPropertyManager;
-	connect(intManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(intManager, &PropertyManager::modified, this, &TextElement::propertyChanged);
 
 	TextPropertyManager *textManager = new TextPropertyManager;
-	connect(textManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(textManager, &PropertyManager::modified, this, &TextElement::propertyChanged);
 
 	ColorPropertyManager *colorManager = new ColorPropertyManager;
-	connect(colorManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(colorManager, &PropertyManager::modified, this, &TextElement::propertyChanged);
 
 	FontPropertyManager *fontManager = new FontPropertyManager;
-	connect(fontManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(fontManager, &PropertyManager::modified, this, &TextElement::propertyChanged);
 
 	Property *visible = new Property(boolManager, tr("Visible"), "visible");
 	visible->setToolTip(tr("Visibilité de l'élément"));

core/videoelement.cpp
@@ -97,7 +97,7 @@ void VideoElement::render(QGraphicsScene *scene, const bool interactive)
 		player = new QMediaPlayer;
 		player->setVideoOutput(item);
 		player->setVolume(getValue("volume").toInt());
-		connect(player, SIGNAL(stateChanged(QMediaPlayer::State)), this, SLOT(stateChanged(QMediaPlayer::State)));
+		connect(player, &QMediaPlayer::stateChanged, this, &VideoElement::stateChanged);
 
 		QMediaPlaylist *playlist = new QMediaPlaylist(player);
 		playlist->addMedia(QUrl::fromLocalFile(getValue("src").toString()));
@@ -110,16 +110,16 @@ void VideoElement::render(QGraphicsScene *scene, const bool interactive)
 PropertyList VideoElement::getProperties() const
 {
 	FilePropertyManager *fileManager = new FilePropertyManager;
-	connect(fileManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(fileManager, &PropertyManager::modified, this, &VideoElement::propertyChanged);
 
 	BoolPropertyManager *boolManager = new BoolPropertyManager;
-	connect(boolManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(boolManager, &PropertyManager::modified, this, &VideoElement::propertyChanged);
 
 	IntSliderPropertyManager *sliderManager = new IntSliderPropertyManager;
-	connect(sliderManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(sliderManager, &PropertyManager::modified, this, &VideoElement::propertyChanged);
 
 	EnumPropertyManager *enumManager = new EnumPropertyManager;
-	connect(enumManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(enumManager, &PropertyManager::modified, this, &VideoElement::propertyChanged);
 
 	Property *group = new Property(0, tr("Vidéo"));
 

plugins/helloworld/helloelement.cpp
@@ -47,7 +47,7 @@ void HelloElement::render(QGraphicsScene *scene, const bool interactive)
 PropertyList HelloElement::getProperties() const
 {
 	StringPropertyManager *stringManager = new StringPropertyManager;
-	connect(stringManager, SIGNAL(modified(QString, QVariant)), this, SLOT(propertyChanged(QString, QVariant)));
+	connect(stringManager, &PropertyManager::modified, this, &HelloElement::propertyChanged);
 
 	Property *group = new Property(0, "Hello World");
 

commit 799bae04866159ce47feb913ce55fa01ed29c63f

fix invalid loadPlugin() slot usage (bug introduced in 55c43b6c49dc9068de811ee25ae48ccde0828154)

core/mainwindow.cpp
@@ -125,7 +125,7 @@ MainWindow::MainWindow(QString commandLineHelp, QString openFile, bool disablePl
 		statusBar()->showMessage(tr("Extensions désactivés par la ligne de commandes."));
 	}
 	else
-		QTimer::singleShot(REFRESH_INTERVAL, this, SLOT(loadPlugins));
+		QTimer::singleShot(REFRESH_INTERVAL, this, SLOT(loadPlugins()));
 }
 
 MainWindow::~MainWindow()